cc14a1f1776acd1df6916e1ede3e2e1562150799
heyolaniran <olaniran.abd@gmail.com>
2024-04-09 23:43:34 +0100

    feat: upload image components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ImageUpload from "@/components/ImageeUpload";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-medium">Main</h1>
+      <ImageUpload />
     </main>
   );
 }
