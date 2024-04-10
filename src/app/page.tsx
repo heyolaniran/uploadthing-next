eece7702c9d53e32bbda0245edc1b5e407d1509c
heyolaniran <olaniran.abd@gmail.com>
2024-04-10 11:38:10 +0100

    feat: finalized main page'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 import ImageUpload from "@/components/ImageeUpload";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-medium">Main</h1>
-      <ImageUpload />
-    </main>
+    <section className="bg-gray-900 text-white">
+      <div className="mx-auto max-w-screen-xl h-screen px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-3xl text-center">
+          <h1
+            className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+          >
+            Upload File to Server Easely 
+
+            <span className="sm:block"> Increase Conversion. </span>
+          </h1>
+
+          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+            numquam ea!
+          </p>
+
+          <div className="mt-4">
+           <ImageUpload />
+          </div>
+
+          
+        </div>
+      </div>
+    </section>
   );
 }
