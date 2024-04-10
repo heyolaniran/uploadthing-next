55e0e292e4ab7615bfc7fb355b73ffc92b9155a1
heyolaniran <olaniran.abd@gmail.com>
2024-04-10 11:39:02 +0100

    feat: files interface

---
+++ src/interfaces/files.ts
@@ -0,0 +1,11 @@
+export interface Files {
+    name : string
+    size : number 
+    type : string
+    key  : string 
+    url  : string
+
+    serverData : {
+        uploadedBy : string
+    }
+}
