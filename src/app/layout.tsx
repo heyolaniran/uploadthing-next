eab386aae83d29d8183362bb7c77ac2a79b1e768
heyolaniran <olaniran.abd@gmail.com>
2024-04-12 22:42:29 +0100

    fix: clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>
-         {children}
-        </ClerkProvider>
        
+         {children}
+        
       </body>
     </html>
   );
